919563c3cc15af0b6cb84abdaf9954c9b2c0b124
Michel <henriquemichel10@gmail.com>
2024-03-18 15:26:30 -0300

    Alterações nas Tabelas

--- api/Data/DataContext.cs
+++ api/Data/DataContext.cs
@@ -1,34 +1,34 @@
-// using System;
-// using api.Controllers;
-// using api.Models;
+using System;
+using api.Controllers;
+using api.Models;
 
-// namespace api.Data
-// {
-//     public class DataContext : DbContext
-//     {
-//         public DataContext(DbContextOptions<DataContext> options) : base(options)
-//         {
+namespace api.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
 
-//         }
-//         public DbSet<Utilizador> TB_UTILIZADOR { get; set; }
+        }
+        public DbSet<Utilizador> TB_UTILIZADOR { get; set; }
             public DbSet<Ecoponto> TB_ECOPONTO { get; set; }
              public DbSet<EcoPoints> TB_ECOPOINTS { get; set; }
 
-//          protected override void OnModelCreating(ModelBuilder modelBuilder)
-//         {
-//             modelBuilder.Entity<Utilizador>().ToTable("Utilizador");
-//             (
-//                 modelBuilder.Entity<Utilizador>().Property(u => u.IdUtilizador).HasColumnName("IdUtilizador"),
-//                 modelBuilder.Entity<Utilizador>().Property(u => u.Nome).HasColumnName("Nome"),
-//                 modelBuilder.Entity<Utilizador>().Property(u => u.Email).HasColumnName("Email"),
-//                 modelBuilder.Entity<Utilizador>().Property(u => u.SituacaoEmail).HasColumnName("SituacaoEmail")
-//             )
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+         {
+            modelBuilder.Entity<Utilizador>().ToTable("Utilizador");
+            (
+                modelBuilder.Entity<Utilizador>().Property(u => u.IdUtilizador).HasColumnName("IdUtilizador"),
+                modelBuilder.Entity<Utilizador>().Property(u => u.Nome).HasColumnName("Nome"),
+                modelBuilder.Entity<Utilizador>().Property(u => u.Email).HasColumnName("Email"),
+                modelBuilder.Entity<Utilizador>().Property(u => u.SituacaoEmail).HasColumnName("SituacaoEmail")
+            )
             modelBuilder.Entity<Ecoponto>().ToTable("Ecoponto");
             (
                 modelBuilder.Entity<Ecoponto>().Property(p => p.IdEcoponto).HasColumnName("IdEcoponto"),
-//              modelBuilder.Entity<Ecoponto>().Property(p => p.Nome).HasColumnName("Nome"),
-//              modelBuilder.Entity<Ecoponto>().Property(p => p.Bairro).HasColumnName("Bairro"),
-//              modelBuilder.Entity<Ecoponto>().Property(p => p.Cidade).HasColumnName("Cidade"),
+             modelBuilder.Entity<Ecoponto>().Property(p => p.Nome).HasColumnName("Nome"),
+             modelBuilder.Entity<Ecoponto>().Property(p => p.Bairro).HasColumnName("Bairro"),
+             modelBuilder.Entity<Ecoponto>().Property(p => p.Cidade).HasColumnName("Cidade"),
                 modelBuilder.Entity<Ecoponto>().Property(p => p.Longitude).HasColumnName("Longitude"),
                 modelBuilder.Entity<Ecoponto>().Property(p => p.Latitude).HasColumnName("Latitude"),
                 modelBuilder.Entity<Ecoponto>().Property(p => p.CNPJ).HasColumnName("CNPJ"),
@@ -41,6 +41,6 @@
                 modelBuilder.Entity<EcoPoints>().Property(e => e.Quantidade).HasColumnName("Quantidade"),
                 modelBuilder.Entity<EcoPoints>().Property(e => e.EcoPointsTotais).HasColumnName("EcoPointsTotais")
             )
-//         }
-//     }
-// }
+        }
+    }
+}
