340f40b26ead70823b420b0119853ba4bd515f51
Michel <henriquemichel10@gmail.com>
2024-03-18 14:56:12 -0300

    Adicionando crud na controller Ecoponto

--- api/Controllers/EcoPontoController.cs
+++ api/Controllers/EcoPontoController.cs
@@ -26,5 +26,38 @@ namespace api.Controllers
         {
             return Ok(ecopontos);
         }
+
+         [HttpPost]
+        public ActionResult<EcopontoModel> Post([FromBody] EcopontoModel ecoponto)
+        {
+            ecoponto.Add(ecopontos);
+            return CreatedAtAction(nameof(Get), new { id = ecoponto.IdEcoponto }, ecoponto);
+        }
+
+        // PUT: api/ecoponto/5
+        [HttpPut("{id}")]
+        public ActionResult<EcopontoModel> Put(int id, [FromBody] EcopontoModel ecoponto)
+        {
+            var index = ecopontos.FindIndex(p => p.IdEcoponto == id);
+            if (index == -1)
+            {
+                return NotFound();
+            }
+            Ecopontoes[index] = ecoponto;
+            return Ok(ecoponto);
+        }
+
+        // DELETE: api/ecoponto/5
+        [HttpDelete("{id}")]
+        public ActionResult Delete(int id)
+        {
+            var ecoponto = Ecopontos.Find(p => p.IdEcoponto == id);
+            if (ecoponto == null)
+            {
+                return NotFound();
+            }
+            Ecopontoes.Remove(ecoponto);
+            return NoContent();
+        }
     }
 }
