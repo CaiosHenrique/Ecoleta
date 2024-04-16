c36a1f76f140a218b064142955ac24a5a767301c
artur <arturdiniz06@gmail.com>
2024-04-15 21:32:59 -0300

    projeto mobile

---
+++ mobile/ECOleta_mobile/ECOleta_mobile/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace ECOleta_mobile
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ mobile/ECOleta_mobile/ECOleta_mobile/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ECOleta_mobile
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
