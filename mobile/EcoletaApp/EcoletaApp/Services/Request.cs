668597f27e1575a3a9f2c52ffa343298b3f851df
-- <-->
2024-04-22 14:52:30 -0300

    correção de bugs

--- mobile/EcoletaApp/EcoletaApp/Services/Request.cs
+++ mobile/EcoletaApp/EcoletaApp/Services/Request.cs
@@ -37,5 +37,47 @@ namespace EcoletaApp.Services
 
             return result;
         }
+
+
+        public async Task<int> PutAsync<TResult>(string uri, TResult data, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization
+            = new AuthenticationHeaderValue("Bearer", token);
+            var content = new StringContent(JsonConvert.SerializeObject(data));
+            content.Headers.ContentType = new MediaTypeHeaderValue("application/json");
+            HttpResponseMessage response = await httpClient.PutAsync(uri, content);
+            string serialized = await response.Content.ReadAsStringAsync();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return int.Parse(serialized);
+            else
+                throw new Exception(serialized);
+        }
+
+        public async Task<TResult> GetAsync<TResult>(string uri, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization
+            = new AuthenticationHeaderValue("Bearer", token);
+            HttpResponseMessage response = await httpClient.GetAsync(uri);
+            string serialized = await response.Content.ReadAsStringAsync();
+            TResult result = await Task.Run(() =>
+            JsonConvert.DeserializeObject<TResult>(serialized));
+            throw new Exception(serialized);
+        }
+
+        public async Task<int> DeleteAsync(string uri, string token)
+        {
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Authorization = new
+           AuthenticationHeaderValue("Bearer", token);
+            HttpResponseMessage response = await httpClient.DeleteAsync(uri);
+            string serialized = await response.Content.ReadAsStringAsync();
+            if (response.StatusCode == System.Net.HttpStatusCode.OK)
+                return int.Parse(serialized);
+            else
+                throw new Exception(serialized);
+        }
+
     }
 }
